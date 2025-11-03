66798d59fc1fee6d52518af7c7443160e8a15b7d
indrakaartikeya <indrakaartikeya778@gmail.com>
2025-11-03 12:44:28 +0530

    refactor: move client files to root for Vercel deployment

--- client/client/src/components/examples/AuthCard.tsx
+++
@@ -1,7 +0,0 @@
-import AuthCard from "../AuthCard";
-
-export default function AuthCardExample() {
-  return (
-    <AuthCard onLogin={(email, password) => console.log("Login:", email, password)} />
-  );
-}
--- client/client/src/pages/Auth.tsx
+++
@@ -1,5 +0,0 @@
-import AuthCard from "@/components/AuthCard";
-
-export default function Auth() {
-  return <AuthCard onLogin={(email, password) => console.log("Login:", email, password)} />;
-}
